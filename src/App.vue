07a14be91472cd4f84285a60ee5f1ac2a718b69e
dengxiangjiu <sd.dengxiangjiu@bhfae.com>
2020-04-23 22:33:42 +0800

    初始化之 删除无用代码 添加CSS文件 等初始化问价

--- src/App.vue
+++ src/App.vue
@@ -1,6 +1,6 @@
 <template>
   <div id="app">
-    <img src="./assets/logo.png">
+<!--    这里显示的内容就是路由对应的组件-->
     <router-view/>
   </div>
 </template>
@@ -12,12 +12,5 @@ export default {
 </script>
 
 <style>
-#app {
-  font-family: 'Avenir', Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
+
 </style>
--- src/main.js
+++ src/main.js
@@ -3,8 +3,14 @@
 import Vue from 'vue'
 import App from './App'
 import router from './router'
+import fastClick from 'fastclick'
+import './assets/style/reset.css'
+import './assets/style/border.css'
 
 Vue.config.productionTip = false
+/*  把fastClick绑定到document.body上 解决点击300毫秒延迟的问题
+ */
+fastClick.attach(document.body)
 
 /* eslint-disable no-new */
 new Vue({
