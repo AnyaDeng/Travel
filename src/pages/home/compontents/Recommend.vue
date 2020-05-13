32cd79e3225f6e3d73a0cd0a2fb15d6999f066b0
dengxiangjiu <sd.dengxiangjiu@bhfae.com>
2020-05-13 09:09:56 +0800

    新增：热门推荐  周末去哪

---
+++ src/pages/home/compontents/Recommend.vue
@@ -0,0 +1,91 @@
+<template>
+  <div>
+    <div class="recommend-title">推荐列表</div>
+    <ul>
+      <li v-for="item of recommendList" :key="item.id">
+        <div class="item border-bottom">
+          <img class="item-img" :src="item.imgUrl" alt="">
+          <div class="info">
+            <p class="info-title">{{item.title}}</p>
+            <p class="info-desc">{{item.des}}</p>
+            <button class="info-button">查看详情</button>
+          </div>
+        </div>
+      </li>
+    </ul>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'HomeRecommend',
+  data () {
+    return {
+      recommendList: [{
+        id: '001',
+        imgUrl: 'https://imgs.qunarzz.com/sight/p0/1507/cc/19733fc0135062788140cbb48ae606a7.water.jpg_224x148_a5c76955.jpg',
+        title: '北京野生动物园',
+        des: '北京郊区的一座大型自然生态动物园'
+      }, {
+        id: '002',
+        imgUrl: 'https://imgs.qunarzz.com/sight/p0/1507/cc/19733fc0135062788140cbb48ae606a7.water.jpg_224x148_a5c76955.jpg',
+        title: '北京野生动物园',
+        des: '北京郊区的一座大型自然生态动物园'
+      }, {
+        id: '003',
+        imgUrl: 'https://imgs.qunarzz.com/sight/p0/1507/cc/19733fc0135062788140cbb48ae606a7.water.jpg_224x148_a5c76955.jpg',
+        title: '北京野生动物园',
+        des: '北京郊区的一座大型自然生态动物园'
+      }, {
+        id: '004',
+        imgUrl: 'https://imgs.qunarzz.com/sight/p0/1507/cc/19733fc0135062788140cbb48ae606a7.water.jpg_224x148_a5c76955.jpg',
+        title: '北京野生动物园',
+        des: '北京郊区的一座大型自然生态动物园'
+      }, {
+        id: '005',
+        imgUrl: 'https://imgs.qunarzz.com/sight/p0/1507/cc/19733fc0135062788140cbb48ae606a7.water.jpg_224x148_a5c76955.jpg',
+        title: '北京野生动物园',
+        des: '北京郊区的一座大型自然生态动物园'
+      }]
+    }
+  }
+}
+</script>
+
+<style lang="stylus" scoped>
+  @import "~styles/varible.styl"
+  .recommend-title
+    margin-top .2rem
+    line-height .8rem
+    background-color #eee
+    text-indent .2rem
+  .item
+    overflow hidden
+    display flex
+    height 1.9rem
+    .item-img
+      width 2.26rem
+      height 1.48rem
+      margin-top .2rem
+      padding 0 .1rem
+    .info
+      overflow hidden
+      flex 1
+      padding .1rem
+      min-width 0
+      .info-title
+        line-height .54rem
+        font-size .32rem
+        ellipsis()
+      .info-desc
+        line-height .44rem
+        color #ccc
+        ellipsis()
+      .info-button
+        line-height .44rem
+        margin-top .16rem
+        background #ff9300
+        padding 0 .2rem
+        border-radius .06rem
+        color #ffffff
+</style>
---
+++ src/pages/home/compontents/Weekend.vue
@@ -0,0 +1,49 @@
+<template>
+  <ul>
+    <li>
+      <div class="title">
+        周末旅游
+      </div>
+      <div class="item-img-test">
+        <img class="item-img" src="http://imgs.qunarzz.com/wugc/p147/201205/30/249ffb73fa960f4193835fbb.jpg_710x360_6c75e1ac.jpg" alt="">
+      </div>
+      <div class="item-info">
+        <p class="item-title">想去哪儿 就去哪儿</p>
+        <p class="item-des">黔灵山公园</p>
+      </div>
+    </li>
+  </ul>
+</template>
+
+<script>
+export default {
+  name: 'HomeWeekend'
+}
+</script>
+
+<style lang="stylus" scoped>
+  @import "~styles/varible.styl"
+  .title
+    text-indent .2rem
+    line-height .8rem
+    background #eee
+  .item-img-test
+    overflow hidden
+    width 100%
+    height 0
+    padding-bottom 50%
+    .item-img
+      width 100%
+  .item-info
+    padding .1rem
+    min-width 0
+    flex 1
+    .item-title
+      line-height .54rem
+      font-size .32rem
+      ellipsis()
+    .item-des
+      line-height .44rem
+      color #ccc
+      ellipsis()
+</style>
